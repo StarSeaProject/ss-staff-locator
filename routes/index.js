67eafc749a4edcb7b48c6415c948720a0e0b8990
ssysm <me@theeditorstudio.com>
2018-05-27 18:12:13 -0400

    格式化代码

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const scope = require('./../sockets/scope');
-/* GET home page. */
+
 router.get('/api/scope', function(req, res, next) {
   res.send(scope);
 });
