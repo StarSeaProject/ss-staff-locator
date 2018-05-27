2d723d9ef2d97e24ffd8b8ecf58fd6b424563642
ssysm <me@theeditorstudio.com>
2018-05-27 18:08:16 -0400

    删除无用的debug命令

--- statics/assets/js/events/clientMode.js
+++ statics/assets/js/events/clientMode.js
@@ -14,7 +14,6 @@ function clientMode() {
                 if (!markers[i] || log.staffList[i].sid !== markers[i].getExtData().sid) {
                     addStaffMarker(0, 0, log.staffList[i].name, log.staffList[i].sid);
                 }
-                console.log(log.staffList[i].sid+","+markers[i].getExtData().sid+i);
                 if (markers[i] && log.staffList[i].sid == markers[i].getExtData().sid && log.staffList[i].locationInfo) {
                     updateMarker(log.staffList[i].location.longitude, log.staffList[i].location.latitude, i, log.staffList[i].sid);
                 }
